6dc247a4f179c1b6ad8693f6368f5dd8586bf7c1
Devikasarath77 <47816479+Devikasarath77@users.noreply.github.com>
2022-02-26 21:35:32 +0530

    Add files via upload

--- SeleniumBasics.java
+++ SeleniumBasics.java
@@ -3,6 +3,7 @@ package selenium.week2.day1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -25,25 +26,48 @@ public class SeleniumBasics {
 	 	WebElement elementLogin = driver.findElement(By.className("decorativeSubmit"));
 		elementLogin.click();
 		
+		//Ctrl + 2 +L to print the web element 
 		
+		//  elementCrm it technical word is variable  
 	
 		WebElement elementCrm	= driver.findElement(By.linkText("CRM/SFA"));
 		elementCrm.click();
 		
 		WebElement elementLeads = driver.findElement(By.linkText("Leads"));
 		elementLeads.click();
+		
 		WebElement elementCreateLead = driver.findElement(By.linkText("Create Lead"));
 		elementCreateLead.click();
 	
 		WebElement elementCompanyName = driver.findElement(By.id("createLeadForm_companyName"));
 		elementCompanyName.sendKeys("P3 solutions");
+		
 		WebElement elementfirstName = driver.findElement(By.id("createLeadForm_firstName"));
 		elementfirstName.sendKeys("Devika");
 		
 		WebElement elementLastName = driver.findElement(By.id("createLeadForm_lastName"));
 		elementLastName.sendKeys("Kaladevi");
+		
+		//Assignment 
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("DEV");
+		
+		driver.findElement(By.name("departmentName")).sendKeys("Testing");
+		
+		driver.findElement(By.name("description")).sendKeys("Testing group");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("Devikask7@gmail.com");
+		
+
+		WebElement elementDropdown = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select dd = new Select(elementDropdown);
+		dd.selectByVisibleText("New York");
 		WebElement elementCreateLeadButton = driver.findElement(By.name("submitButton"));
 		elementCreateLeadButton.click();
+		
+		System.out.println("The Title of the Resulting page is:" +driver.getTitle());
+		
+		
+		
+		
 		
 		
 
